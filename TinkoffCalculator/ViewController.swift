9e97effbe26da3d2185da8592ea0280d77d99b77
Denese Glynda <denesegl@mi-d6.msk.21-school.ru>
2024-06-06 15:43:37 +0300

    add Operation

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
+enum Operation {
+    case add
+    case substract
+    case multiply
+    case divide
+}
+
+enum CalculationHistoryItem {
+    case number(Double)
+    case operation(Operation)
+}
+
 class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        guard let buttonText = sender.currentTitle else { return
-        }
+        guard let buttonText = sender.currentTitle else { return }
+
+        label.text = buttonText
         print(buttonText)
     }
+    
+    @IBAction func operationbuttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return
+            }
+
+        label.text = buttonText
+        print(buttonText)
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
